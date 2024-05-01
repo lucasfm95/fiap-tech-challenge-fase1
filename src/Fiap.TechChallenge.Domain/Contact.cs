37353309640c9fd701b3b50874ddcfa017ada188
Allan Santos <allan.santos@tinnova.com.br>
2024-05-01 10:33:56 -0300

    Contact and launchSettings.json

--- src/Fiap.TechChallenge.Domain/Contact.cs
+++ src/Fiap.TechChallenge.Domain/Contact.cs
@@ -1,6 +1,11 @@
 namespace Fiap.TechChallenge.Domain;
 
-public class Contact
+public class Contact(string name, string email, string phoneNumber, int dddNumber)
 {
-    
+    public long Id { get; set; }
+    public string Name { get; set; } = name;
+    public string Email { get; set; } = email;
+    public string PhoneNumber { get; set; } = phoneNumber;
+
+    public int DddNumber { get; set; } = dddNumber;
 }
