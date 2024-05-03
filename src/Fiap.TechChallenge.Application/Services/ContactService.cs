97c7b60c766b3f62bd88937cf35cf00875622580
ronan strobel <ronanstrobel@ronans-MacBook-Pro.local>
2024-05-02 20:18:31 -0400

    Add Validations

--- src/Fiap.TechChallenge.Application/Services/ContactService.cs
+++ src/Fiap.TechChallenge.Application/Services/ContactService.cs
@@ -1,6 +1,7 @@
 using Fiap.TechChallenge.Domain.Entities;
 using Fiap.TechChallenge.Domain.Repositories;
 using Fiap.TechChallenge.Domain.Request;
+using FluentValidation;
 
 namespace Fiap.TechChallenge.Application.Services;
 
@@ -8,7 +9,9 @@ public class ContactService(IContactRepository contactRepository)
 {
     public async Task<long> CreateAsync(ContactPostRequest request, CancellationToken cancellationToken)
     {
-        //TODO: Validar se ddd existe
+        var validator = new ContactPostRequestValidator();
+        await validator.ValidateAndThrowAsync(request, cancellationToken);
+        
         var contact = new Contact(request.Name,request.Email, request.PhoneNumber, request.Ddd);
         var result = await contactRepository.CreateAsync(contact, cancellationToken);
         return result;
--- src/Fiap.TechChallenge.Domain/Request/ContactPostRequest.cs
+++ src/Fiap.TechChallenge.Domain/Request/ContactPostRequest.cs
@@ -1,3 +1,5 @@
+using FluentValidation;
+
 namespace Fiap.TechChallenge.Domain.Request;
 
 public class ContactPostRequest(short ddd, string email, string phoneNumber, string name)
@@ -6,4 +8,20 @@ public class ContactPostRequest(short ddd, string email, string phoneNumber, str
     public string Email { get; init; } = email;
     public string PhoneNumber { get; init; } = phoneNumber;
     public string Name { get; init; } = name;
+}
+
+public class ContactPostRequestValidator : AbstractValidator<ContactPostRequest>
+{
+    public ContactPostRequestValidator()
+    {
+        RuleFor(x => x.Ddd).NotEmpty().WithMessage("DDD is required");
+        RuleFor(x => x.Ddd).LessThan((short)100).WithMessage("invalid ddd number");
+        RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required");
+        RuleFor(x => x.Email).EmailAddress().WithMessage("Invalid email format");
+        RuleFor(x => x.PhoneNumber).NotEmpty().WithMessage("PhoneNumber is required");
+        RuleFor(x => x.PhoneNumber).Length(9).WithMessage("Phone number must contain 9 digits");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(x => x.Name).MaximumLength(200);
+        RuleFor(x => x.Name).MinimumLength(3);
+    }
 }
