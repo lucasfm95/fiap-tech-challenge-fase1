ee54fa5c75bfecb79b8b434a459ae8f541fa5cde
Allan Santos <allan.santos@tinnova.com.br>
2024-05-24 14:42:59 -0300

    - Exemplo para subir o banco local com docker e EF Core.
    docker-compose up -d
    
    cd .\src\Fiap.TechChallenge.Infrastructure\
    
    dotnet ef migrations add InitialMigration
    
    dotnet ef database update

--- src/Fiap.TechChallenge.Infrastructure/Context/ContactDbContext.cs
+++ src/Fiap.TechChallenge.Infrastructure/Context/ContactDbContext.cs
@@ -4,10 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Fiap.TechChallenge.Infrastructure.Context;
 
-public class ContactDbContext(DbContextOptions<ContactDbContext> options) : DbContext(options)
+public class ContactDbContext : DbContext
 {
     public DbSet<Contact> Contacts { get; set; }
     public DbSet<DddState> DddStates { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=123456");
+    }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
